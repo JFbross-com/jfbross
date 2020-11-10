40fd34c5a0388f29f3b53086ec98cd4ab0a1bd15
ParhamJF <parham_jafary@yahoo.com>
2020-11-10 22:44:53 +0100

    start

---
+++ build/precache-manifest.c8da676ebfdd7279aded202bbec8ab72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b21b98787ec587f93d1d5077a5252e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a7a05071db94fcc2750",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a5b104c73ae42d6cb28d",
+    "url": "/static/js/2.29eb80a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.29eb80a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a7a05071db94fcc2750",
+    "url": "/static/js/main.0826a90b.chunk.js"
+  },
+  {
+    "revision": "e22fd322f0b45aec7fb4",
+    "url": "/static/js/runtime-main.a7f6340d.js"
+  }
+]);
